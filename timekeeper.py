77d8e9957764787e9fdafcf5418f59bbea119a74
spech13 <es-popov2@yandex.ru>
2023-09-17 22:27:06 +0300

    Black formating

--- timekeeper.py
+++ timekeeper.py
@@ -1,18 +1,24 @@
 from datetime import datetime
 from time import sleep
 
+
 def timekeeper_decorator(func):
     def wrapper(*args, **kwargs):
         before = datetime.now()
         result = func(*args, **kwargs)
-        print(f"{func.__name__} applied for {(datetime.now() - before).total_seconds()} seconds")
+        print(
+            f"{func.__name__} applied for {(datetime.now() - before).total_seconds()} seconds"
+        )
         return result
+
     return wrapper
+
 
 @timekeeper_decorator
 def quick_arithmetic_sum(sequence):
     count = len(sequence)
-    return (sequence[0] + sequence[count-1]) * count / 2
+    return (sequence[0] + sequence[count - 1]) * count / 2
+
 
 @timekeeper_decorator
 def arithmetic_sum(sequence):
@@ -21,9 +27,9 @@ def arithmetic_sum(sequence):
         summ += num
     return summ
 
+
 count_range = 10000000
 list_numbers = [num for num in range(1, count_range)]
 
 print(quick_arithmetic_sum(list_numbers))
 print(arithmetic_sum(list_numbers))
-
